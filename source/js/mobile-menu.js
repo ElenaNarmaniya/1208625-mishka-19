b4c353885596b2a303f609ba86eb46199696335c
ElenaNarmaniya <narmallada@gmail.com>
2020-05-22 02:09:54 +0300

    изменения на 2-09

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,9 +1,9 @@
-  var burger = document.querySelector(".main-nav__burger");
+  var burger = document.querySelector(".burger");
   var nav = document.querySelector(".main-nav");
   nav.classList.remove("main-nav--no-js");
   burger.onclick = function() {
-    burger.classList.toggle("main-nav__burger--closed");
-    burger.classList.toggle("main-nav__burger--opened");
+    burger.classList.toggle("burger__nav--closed");
+    burger.classList.toggle("burger__nav--opened");
     nav.classList.toggle("main-nav--opened");
     nav.classList.toggle("main-nav--closed");
   };
